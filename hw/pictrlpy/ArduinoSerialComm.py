765f5985c4782b62192002d53f09f842d64957b9
plainswalker <plainswalker615@gmail.com>
2018-05-13 12:41:36 +0900

    update arduino communication

--- hw/pictrlpy/ArduinoSerialComm.py
+++ hw/pictrlpy/ArduinoSerialComm.py
@@ -3,10 +3,15 @@ import serial
 serialcomm = serial.Serial('/dev/ttyACM0', 9600)
 
 MAX_BUF = 20
-ardubuff = []
+ardubuff = [None] * MAX_BUF
 buffidx = 0
 
 while True:
-    ardubuff[buffidx] = serialcomm.readline()
-    print(buffidx)
-    buffidx = (buffidx + 1) % MAX_BUF
+    try:
+        ardubuff[buffidx] = serialcomm.readline()
+        print(ardubuff[buffidx])
+
+        buffidx = (buffidx + 1) % MAX_BUF
+    except KeyboardInterrupt as ctrlc:
+        print(ctrlc)
+        exit()
